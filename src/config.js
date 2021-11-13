6fc1f6c6343873005c3ee41a2e88272fa2ee68a9
Dennis Mubamba <dennism501@outlook.com>
2021-11-13 23:35:44 +0200

    :construction: Added upload method

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const photoBucket = process.env.AWS_BUCKET_NAME_UPLOAD
+export const awsPhotoBucket = process.env.AWS_BUCKET_NAME_UPLOAD
+export const awsAccessKey = process.env.AWS_ACCESS_KEY_ID
+export const awsSecretKey = process.env.AWS_SECRET_ACCESS_KEY
--- src/services.js
+++ src/services.js
@@ -1,8 +1,9 @@
 import AWS from "aws-sdk";
+import { awsPhotoBucket,awsAccessKey,awsSecretKey } from 'src/config/config.js';
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: awsAccessKey,
+  secretAccessKey: awsSecretKey,
 });
 
 //=====READ AND WRITE======
@@ -11,6 +12,14 @@ const uploadFile = (fileName) => {
     const fileContent = fs.readFileSync(fileName);
 
     const params = {
-        
+        Bucket: awsPhotoBucket,
+        Key: filename,
+        Body: fileContent
     }
+
+    s3.upload(params, (err, data) => {
+        if(err) reject(err);
+        
+        return resolve(data);
+    })
 }
